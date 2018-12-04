ed79a14f57b986c81a56213f71c37e9dc4ddd438
Chengyu Sun <csun@calstatela.edu>
2018-12-04 11:07:44 -0800

    Added routing

--- src/app/add-entry/add-entry.component.ts
+++ src/app/add-entry/add-entry.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { GuestBookEntry } from 'src/models/GuestBookEntry';
 import { DataService } from '../data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-entry',
@@ -8,11 +9,12 @@ import { DataService } from '../data.service';
   styles: []
 })
 export class AddEntryComponent implements OnInit {
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private router: Router) {}
 
   ngOnInit() {}
 
   addEntry(name: HTMLInputElement, message: HTMLInputElement) {
     this.dataService.addEntry(new GuestBookEntry(name.value, message.value));
+    this.router.navigate(['']);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { GuestBookComponent } from './guest-book/guest-book.component';
 import { AddEntryComponent } from './add-entry/add-entry.component';
 
+const routes: Routes = [
+  { path: '', component: GuestBookComponent },
+  { path: 'AddEntry', component: AddEntryComponent }
+];
+
 @NgModule({
   declarations: [AppComponent, GuestBookComponent, AddEntryComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
